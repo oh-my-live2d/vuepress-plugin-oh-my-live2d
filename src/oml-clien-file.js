babb2530cf2371a84cf6d771fbada2aff101ffbd
loclink <loclink@163.com>
2023-01-05 14:20:02 +0800

    chore: 修改导入路径

--- src/oml-clien-file.js
+++ src/oml-clien-file.js
@@ -1,8 +1,8 @@
 import { defineClientConfig } from '@vuepress/client';
-const { loadLive2DModel } = require('oh-my-live2d')
+import { loadLive2DModel } from 'oh-my-live2d/dist/index.umd';
 const oml = loadLive2DModel(omlConfig);
 export default defineClientConfig({
-    enhance({ app }) {
-        app.config.globalProperties.$oml = oml;
-    }
+  enhance({ app }) {
+    app.config.globalProperties.$oml = oml;
+  }
 });
