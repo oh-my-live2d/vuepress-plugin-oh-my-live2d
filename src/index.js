2aa263b770fe0656dd100905c0b6d7f1553be058
wangchonglin <735761545@qq.com>
2023-02-06 16:10:52 +0800

    fix: windows运行项目时路径错误bug

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@ const __dirname = path.dirname(__filename);
 
 const ohmylive2dPlugin = (options = {}) => {
   return function () {
+    const omlClienFilePath = path.resolve(__dirname, './oml-clien-file.js').replaceAll('\\', '/');
     return {
       name: 'vuepress-plugin-oh-my-live2d',
       define: {
         omlOptions: options
       },
       multiple: false,
-      clientConfigFile: path.resolve(__dirname, './oml-clien-file.js')
+      clientConfigFile: omlClienFilePath
     };
   };
 };
